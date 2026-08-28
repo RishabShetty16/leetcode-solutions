a06f34bb879d96892e2265b7a12d9adbbdeee4d1
Rishab Shetty <172990099+RishabShetty16@users.noreply.github.com>
2026-08-28 18:09:07 +0530

    Time: 3 ms (60.6%), Space: 19.1 MB (96.52%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n=len(cost)
+        dp=[0]*(n+1)
+        #I can start at stair 0 or 1 
+        for i in range(2,n+1):
+            one_step=dp[i-1]+cost[i-1]
+            two_step=dp[i-2]+cost[i-2]
+            dp[i]=min(one_step,two_step)
+        return dp[n]
+
